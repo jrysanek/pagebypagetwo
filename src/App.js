c27c057c6850c83c4c94b9c177a63a77e5d42726
Jenna Rysanek <jennarysanek@jennas-mbp.lan>
2021-01-03 12:21:27 -0500

    Api Call to call information

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [input, userInput] = useState('')
+  const [arr, setArr] = useState([])
 
 
 
+const getBooks = async () => {
+ 
+  let apiKey = process.env.REACT_APP_API_KEY
+  const response = await axios(`https://www.googleapis.com/books/v1/volumes?q=flowers&orderBy=newest&key=${apiKey}`)
+
+  setArr(response)
+}
 
 
+
+useEffect(() => {
+  getBooks();
+  console.log('hello world')
+}, [])
 
 
 
