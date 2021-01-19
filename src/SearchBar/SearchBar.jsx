efc5201d26ca33b4e98c9097ec8663c1f939aefa
Jenna Rysanek <jennarysanek@jennas-mbp.lan>
2021-01-19 15:29:53 -0500

    Added a newSearch function to clear old search bar data on click

--- src/SearchBar/SearchBar.jsx
+++ src/SearchBar/SearchBar.jsx
@@ -2,23 +2,35 @@ import React, { useState } from 'react'
 
 export default function SearchBar(props) {
     const [search, setSearch] = useState('')
+    // const [newSearch, setNewSearch] = useState('')
 
     const handleChange = (e) => {
         e.preventDefault();
         props.setInput(search)
         props.setIsLoaded(true)
+
+
+        const newSearch = () => {
+            setSearch('')
+        }
+        newSearch()
     }
+
+
+
 
 
     return (
         <form onSubmit={handleChange} className="search-bar">
             <label htmlFor="search"></label>
             <input
-             onChange={e => setSearch(e.target.value)}
-             value={search} 
-            //  for="search" 
-             type="text" />      
-             
+                onChange={e => setSearch(e.target.value)}
+                value={search}
+                //  for="search" 
+                type="text"
+            />
+
+
             <input type="submit" value="submit" />
         </form>
     )
