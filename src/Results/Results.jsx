0b2b99a6e193000f4c4b83c8ea6adcc269bcb706
Jenna Rysanek <jennarysanek@jennas-mbp.lan>
2021-03-15 13:19:41 -0400

    Styled book results Added image sizes and set base style

--- src/Results/Results.jsx
+++ src/Results/Results.jsx
@@ -11,12 +11,12 @@ export default function Results(props) {
         <div className="results">
             {bookList.map((x, index) => {
                 return (
-                    <>
+                    <div className="book">
                     <Link to={`/Showbook/${index}`}>
                         <img src={x.volumeInfo.imageLinks.smallThumbnail} key={index}  alt={x.volumeInfo.title} />
                      </Link>
-                        <h1 key={x.volumeInfo.title}>{x.volumeInfo.title}</h1>
-                    </>
+                        <h2 key={x.volumeInfo.title}>{x.volumeInfo.title}</h2>
+                    </div>
                 )
             })}
         </div>
